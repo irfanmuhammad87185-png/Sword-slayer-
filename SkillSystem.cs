585f794c2a1357487b2e731179284b652edc8049
irfanmuhammad87185-png <irfanmuhammad87185@gmail.com>
2026-03-06 02:19:58 +0700

    Create SkillSystem.cs with Q (slash) and E (heal) abilities

---
+++ SkillSystem.cs
@@ -0,0 +1,16 @@
+// SkillSystem.cs
+
+public class SkillSystem 
+{
+    public void SkillQ() // Slash Ability
+    {
+        // Implement the Slash ability logic here
+        Console.WriteLine("Slash ability activated!");
+    }
+
+    public void SkillE() // Heal Ability
+    {
+        // Implement the Heal ability logic here
+        Console.WriteLine("Heal ability activated!");
+    }
+}
